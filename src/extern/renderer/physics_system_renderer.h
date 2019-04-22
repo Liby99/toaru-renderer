68785e490b350b91267cf0a5f21aadf77d657ec9
Liby Lee <liby99@icloud.com>
2019-04-21 18:56:34 -0700

    Compiling renderer

--- src/extern/renderer/physics_system_renderer.h
+++ src/extern/renderer/physics_system_renderer.h
@@ -1,4 +1,15 @@
 #ifndef TOARU_PHYSICS_SYSTEM_RENDERER_H
 #define TOARU_PHYSICS_SYSTEM_RENDERER_H
 
+#include "core/renderer.h"
+#include "core/material.h"
+#include "physics/physics_system.h"
+
+namespace toaru {
+  class PhysicsSystemRenderer : public Renderer {
+  public:
+    virtual void render();
+  };
+}
+
 #endif
--- src/intern/renderer/physics_system_renderer.cpp
+++ src/intern/renderer/physics_system_renderer.cpp
@@ -1,1 +1,72 @@
 #include "renderer/physics_system_renderer.h"
+
+using namespace toaru;
+
+void PhysicsSystemRenderer::render() {
+  if (object().hasComponent("system")) {
+    PhysicsSystem &sys = object().getComponent<PhysicsSystem>("system");
+    if (object().hasComponent("material")) {
+      Material &mat = object().getComponent<Material>("material");
+
+      // Bind the shader
+      Shader &shader = mat.getShader();
+      shader.bind();
+
+      // Get the transform matrices
+      shader.setUniform("model", object().transform.getTransform());
+      shader.setUniform("viewProj", context().getMainCameraViewProj());
+
+      // First go through indices
+      int maxFaceAmount = sys.faces.size();
+      int faceAmount = 0, vertAmount = 0;
+      MatrixXu indices(3, maxFaceAmount);
+      MatrixXf positions(3, maxFaceAmount * 3);
+      MatrixXf normals(3, maxFaceAmount * 3);
+
+      // Go through
+      for (int i = 0; i < sys.faces.size(); i++) {
+        if (!sys.faces[i]->isInternalFace()) {
+
+          // First get all points
+          auto p1 = sys.faces[i]->points[0];
+          auto p2 = sys.faces[i]->points[1];
+          auto p3 = sys.faces[i]->points[2];
+
+          // Then get the normal
+          Vector3f n = sys.faces[i]->normal;
+
+          // Add a face
+          indices.col(faceAmount) << vertAmount, vertAmount + 1, vertAmount + 2;
+          faceAmount += 1;
+
+          // Add three normals
+          normals.col(vertAmount) = n;
+          normals.col(vertAmount + 1) = n;
+          normals.col(vertAmount + 2) = n;
+
+          // Add three positions
+          positions.col(vertAmount) = p1->position;
+          positions.col(vertAmount + 1) = p2->position;
+          positions.col(vertAmount + 2) = p3->position;
+
+          // Increment the vertices
+          vertAmount += 3;
+        }
+      }
+
+      // Resize the positions and normals
+      indices.conservativeResize(3, faceAmount);
+      positions.conservativeResize(3, vertAmount);
+      normals.conservativeResize(3, vertAmount);
+
+      // Pass to shader
+      shader.uploadIndices(indices);
+      shader.uploadAttr("position", positions);
+      shader.uploadAttr("normal", normals);
+
+      // Material
+      mat.prerender();
+      shader.drawIndexed(GL_TRIANGLES, 0, indices.cols());
+    }
+  }
+}
